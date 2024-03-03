cf03ae93d04985f3c7650631b9a625c819dd5e5c
Aidan Shilling <aidanshilling@disroot.org>
2024-03-03 16:32:15 -0600

    Add start of parser & ast

---
+++ ast/ast.go
@@ -0,0 +1,54 @@
+package ast
+
+import "monkey/interpreter/token"
+
+// -----
+// Nodes
+// -----
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type LetStatement struct {
+	Token token.Token // token.LET
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// -------
+// Program
+// -------
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
