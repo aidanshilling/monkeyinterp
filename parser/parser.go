5370d791b17f9bb4d582a93e7db9dc8af18be72f
Aidan Shilling <aidanshilling@disroot.org>
2024-03-05 22:11:45 -0600

    Add start of parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,33 @@
 package parser
+
+import (
+	"monkey/interpreter/ast"
+	"monkey/interpreter/lexer"
+	"monkey/interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// reads two tokens so curToken & peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
